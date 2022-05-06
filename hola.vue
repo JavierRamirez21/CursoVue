83f1233ced3afc6eb6b95e2eb4240cfb60313b00
Javier Ramirez <javier.upes21@gmail.com>
2022-05-05 23:47:04 -0600

    agregue las clases faltantes

---
+++ hola.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <p>Lorem ipsum dolor</p>
+    <div>{{text}}</div>
+</div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                text: "Hola!"
+            }
+        }
+    };
+</script>
+
+<style >
+p{
+    color: blue;
+}
+</style>
